76ffda0915472fdc5132e00634d2cfd7a8a00bcf
Ahmad <cp.chahmad@gmail.com>
2019-01-07 13:10:23 +0500

    'add'

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Oseintow\Shopify\ShopifyServiceProvider::class,
+        RocketCode\Shopify\ShopifyServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -223,7 +225,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Shopify' => Oseintow\Shopify\Facades\Shopify::class,
+        
     ],
 
 ];
